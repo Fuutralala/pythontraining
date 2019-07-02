05db0387f926c3f11eb9e9a460c8cb4bdee91629
entwederoder <entwederoder9@gmail.com>
2019-07-02 21:55:40 +0200

    Did someone say oneliner? Shortened the code to one line with the help of one semicolon

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,3 +1,1 @@
-import random, string
-print(''.join(random.choice(string.ascii_letters + string.digits + string.punctuation
-) for i in range(int(input("Enter how long your password should be: \n>")))))
+import random, string; print(''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(int(input("Enter how long your password should be: \n>")))))
